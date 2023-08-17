9e9d64fa442b64da5b87a677e5a4c3576a701881
redavillm <leonofforc@yandex.ru>
2023-08-18 01:31:56 +0300

    start project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <h1>Tasks list</h1>
+        <div className="menu"></div>
+        <div className="list">
+          <div className="task"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
