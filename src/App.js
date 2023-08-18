cb434f7f0e4da0d630b9e4a94a5b2e5e3a680a81
redavillm <leonovvoleg@yandex.ru>
2023-08-18 19:15:34 +0300

    создал размметку

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import styles from "./App.module.css";
 
 function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    fetch("https://mocki.io/v1/8f3e2fab-7aca-4163-b5be-ea48fedcc5f5")
+      .then((loadedData) => loadedData.json())
+      .then((loadedTasks) => setTasks(loadedTasks));
+  });
+
   return (
-    <div className="App">
-      <div className="wrapper">
-        <h1>Tasks list</h1>
-        <div className="menu"></div>
-        <div className="list">
-          <div className="task"></div>
+    <div className={styles.app}>
+      <h1>Tasks list</h1>
+      <div className={styles.wrapper}>
+        <div className={styles.menu}>
+          <button className={styles.menu_btn}>Sort by ABC</button>
+          {/* <button className={styles.menu_btn}>+ Add new task +</button> */}
+          <form>
+            <input className={styles.menu_search}></input>
+            <button className={styles.menu_btn}>Search</button>
+          </form>
+        </div>
+        <div className={styles.list}>
+          {tasks.map(({ id, text }) => (
+            <div id={id} className={styles.task}>
+              {id + ". "}
+              {text}
+              <button className={styles.del_btn}>X</button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
