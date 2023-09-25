4ccbff5e5acf099c0d06f173cf95099907671ba1
redavillm <leonofforc@yandex.ru>
2023-09-26 00:29:11 +0300

    откоректировал фильтрацию и сортировку

--- src/scripts.js
+++ src/scripts.js
@@ -10,8 +10,8 @@ export const createTasksList = ({ tasks, setIsLoading }) => {
 
 export const findTasksList = ({ findingTask, tasks, setIsLoading }) => {
   return Object.entries(tasks)
-    .filter((task) => task.text.includes(findingTask))
-    .map(({ id, text }, index) => (
+    .filter((task) => task[1].text.includes(findingTask))
+    .map(([id, { text }], index) => (
       <div>
         <Task id={id} text={text} index={index} setIsLoading={setIsLoading} />
       </div>
@@ -20,12 +20,12 @@ export const findTasksList = ({ findingTask, tasks, setIsLoading }) => {
 
 export const sortTasksList = ({ tasks, setIsLoading }) => {
   return Object.entries(tasks)
-    .sort(function (a, b) {
-      let x = a.text ? a.text.toLowerCase() : "";
-      let y = b.text ? b.text.toLowerCase() : "";
+    .sort((a, b) => {
+      let x = a[1].text ? a[1].text.toLowerCase() : "";
+      let y = b[1].text ? b[1].text.toLowerCase() : "";
       return x < y ? -1 : x > y ? 1 : 0;
     })
-    .map(({ id, text }, index) => (
+    .map(([id, { text }], index) => (
       <div>
         <Task id={id} text={text} index={index} setIsLoading={setIsLoading} />
       </div>
